b71c1a50dc0701ac950c36d951d5aa1c9a0c73b1
Franco Osores <franco_osores@hotmail.com>
2022-06-17 20:48:07 -0300

    Feat: Created NavBar, Footer and public/login component

--- PokeDexLite/src/app/app.module.ts
+++ PokeDexLite/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './features/components/nav-bar/nav-bar.component';
+import { FooterComponent } from './features/components/footer/footer.component';
+import { LoginComponent } from './features/pages/public/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
