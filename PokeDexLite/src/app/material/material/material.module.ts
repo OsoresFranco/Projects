d3606a15ae6e945e1db555ab306b4b04ee509462
Franco Osores <franco_osores@hotmail.com>
2022-06-18 18:26:39 -0300

    Feat: NavBar layout finished

--- PokeDexLite/src/app/material/material/material.module.ts
+++ PokeDexLite/src/app/material/material/material.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -12,12 +14,16 @@ import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
     CommonModule,
     MatButtonModule,
     MatInputModule,
-    MatBottomSheetModule
+    MatBottomSheetModule,
+    MatMenuModule,
+    MatIconModule
   ],
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatBottomSheetModule
+    MatBottomSheetModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
