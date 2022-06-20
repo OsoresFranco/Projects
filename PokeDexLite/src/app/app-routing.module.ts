ee5d6120a44029f897dd05c4fc8238f4ead0efa3
Franco Osores <franco_osores@hotmail.com>
2022-06-20 07:07:24 -0300

    Login route

--- PokeDexLite/src/app/app-routing.module.ts
+++ PokeDexLite/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonListComponent } from './features/pages/private/pokemon-list/pokemon-list.component';
+import { LoginComponent } from './features/pages/public/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  'login', component:  LoginComponent},
+  { path:  'home', component:  PokemonListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
